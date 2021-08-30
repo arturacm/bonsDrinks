6c61ed55cdde6be5550a0df50fc915efc92015e6
Artur Moreira <arturacm@gmail.com>
2021-08-30 11:25:59 -0300

    implementação do uso da useRef

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,21 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 
 function Form () {
+
+    
+
+    const novaRef = useRef(null)
+    useEffect(()=>{
+        novaRef.current.addEventListener("mouseover",()=>{
+            if(novaRef.current.style.backgroundColor == "red"){
+                novaRef.current.style.backgroundColor = "blue"
+            }else{
+                novaRef.current.style.backgroundColor = "red"
+            }
+            
+        })
+    },[])
+
     const [label, setLabel] = useState("Teste")
     const novoValor = (event)=> {
         setLabel(event.target.value)
@@ -8,6 +23,7 @@ function Form () {
     }
     return(
         <form className="formulario">
+            <h1 ref={novaRef}>Teste da ref</h1>
         <input type="text" onChange={novoValor} placeholder="testando aqui" />
         {/* <input type="text" placeholder="Função" />
         <input type="text" placeholder="braba" /> */}
